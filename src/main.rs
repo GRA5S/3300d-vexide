d5e0ba3f97539ab6a8e6fa7543614af834b1afac
grass grass <thegrassmanreplit@gmail.com>
2026-01-30 13:55:47 -0700

    fgyuaoifdugyiu cghanged the ntake and the uhhh piston midgoal and good

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,16 @@ use evian::{
 };
 use vexide::prelude::*;
 use vexide::adi::digital::LogicLevel;
+use vexide_motorgroup::MotorGroup;
 
 struct Robot {
     controller: Controller,
     drivetrain: Drivetrain<Differential, WheeledTracking>,
-    intake: (Motor, Motor, Motor),
+    intake: MotorGroup,
     wing: AdiDigitalOut,
     matchload: AdiDigitalOut,
-    triple_state: (AdiDigitalOut, AdiDigitalOut),
+    hood: AdiDigitalOut,
+    midgoal: AdiDigitalOut,
 }
 pub const TRACK_WIDTH: f64 = 10.0;
 pub const WHEEL_DIAMETER: f64 = 3.25;
@@ -29,23 +31,23 @@ pub const GEARING: f64 = 48.0/72.0;
 impl Robot {
     const LINEAR_PID: Pid = Pid::new(1.0, 0.0, 0.125, None);
     const LATERAL_PID: Pid = Pid::new(0.09, 0.001, 0.004, Some(2.0));   
-    const ANGULAR_PID: AngularPid = AngularPid::new(10.0, 0.0, 1.29, None);
+    const ANGULAR_PID: AngularPid = AngularPid::new(16.0, 0.0, 1.0, None);
     const LINEAR_TOLERANCES: Tolerances = Tolerances::new()
         .error(4.0)
         .velocity(0.25)
         .duration(Duration::from_millis(15));
     const ANGULAR_TOLERANCES: Tolerances = Tolerances::new()
-        .error(f64::to_radians(0.9))
+        .error(f64::to_radians(8.0))
         .velocity(0.09)
-        .duration(Duration::from_millis(150));
+        .duration(Duration::from_millis(15));
 }
 
 impl Compete for Robot {
     async fn autonomous(&mut self) {
         let dt = &mut self.drivetrain;
         let mut seeking = Seeking {
-            linear_controller: Self::LINEAR_PID,
-            lateral_controller: Self::LATERAL_PID,
+            linear_controller: Pid::new(0.0, 0.0, 0.0, None),
+            lateral_controller: Pid::new(0.0, 0.0, 0.0, None),
             tolerances: Self::LINEAR_TOLERANCES,
             timeout: Some(Duration::from_secs(10)),
         };
@@ -60,16 +62,11 @@ impl Compete for Robot {
         // Turn to 0 degrees heading.
         basic.turn_to_heading(dt, 0.0.deg()).await;
         basic.turn_to_heading(dt, 90.0.deg()).await;
-        // basic.turn_to_heading(dt, 0.0.deg()).await;
-        // basic.turn_to_heading(dt, 90.0.deg()).await;
-        // basic.turn_to_heading(dt, 0.0.deg()).await;
-        // basic.drive_distance(dt, 24.0).await;
-        // basic.turn_to_heading(dt, 180.0.deg()).await;
-        // basic.drive_distance(dt, 24.0).await;
+        basic.turn_to_heading(dt, 0.0.deg()).await;
         
         // // Move to point (24, 24) on the field.
-        // seeking.move_to_point(dt, (24.0, 24.0)).await;
-        
+        seeking.move_to_point(dt, (24.0, 24.0)).await;
+
 
     }
 
@@ -85,31 +82,21 @@ impl Compete for Robot {
 
 
             if state.button_r1.is_pressed() {
-                _ = self.intake.0.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.1.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.2.set_voltage(Motor::V5_MAX_VOLTAGE);
-                // _ = self.triple_state.0.set_high();
-                // _ = self.triple_state.1.set_high();
+                _ = self.intake.set_voltage(-Motor::V5_MAX_VOLTAGE);
             } else if state.button_l2.is_pressed() {
-                _ = self.intake.0.set_voltage(Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.1.set_voltage(Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.2.set_voltage(Motor::V5_MAX_VOLTAGE);
+                _ = self.intake.set_voltage(Motor::V5_MAX_VOLTAGE);
+                _ = self.hood.set_high();
+                _ = self.midgoal.set_high();
             } else if state.button_l1.is_pressed() {
-                _ = self.intake.0.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.1.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.2.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.triple_state.0.set_low();
-                _ = self.triple_state.1.set_low();
+                _ = self.intake.set_voltage(Motor::V5_MAX_VOLTAGE);
+                _ = self.hood.set_low();
+                _ = self.midgoal.set_high();
              } else if state.button_r2.is_pressed() {
-                _ = self.intake.0.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.1.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.intake.2.set_voltage(-Motor::V5_MAX_VOLTAGE);
-                _ = self.triple_state.0.set_high();
-                _ = self.triple_state.1.set_low();
+                _ = self.intake.set_voltage(Motor::V5_MAX_VOLTAGE);
+                _ = self.hood.set_low();
+                _ = self.midgoal.set_low();
             } else {
-                _ = self.intake.0.set_voltage(0.0);
-                _ = self.intake.1.set_voltage(0.0);
-                _ = self.intake.2.set_voltage(0.0);
+                _ = self.intake.set_voltage(0.0);
             }
 
 
@@ -159,17 +146,14 @@ async fn main(peripherals: Peripherals) {
                 Some(imu),
             ),
         ),
-        intake: (
-            Motor::new(peripherals.port_6, Gearset::Blue, Direction::Reverse),
-            Motor::new(peripherals.port_7, Gearset::Blue, Direction::Reverse),
+        intake: MotorGroup::new(vec![
+            Motor::new(peripherals.port_6, Gearset::Blue, Direction::Forward),
             Motor::new(peripherals.port_5, Gearset::Blue, Direction::Reverse),
-        ),
+        ]),
         matchload: AdiDigitalOut::with_initial_level(peripherals.adi_b, LogicLevel::Low),
         wing: AdiDigitalOut::with_initial_level(peripherals.adi_d, LogicLevel::Low),
-        triple_state: (
-            AdiDigitalOut::with_initial_level(peripherals.adi_e, LogicLevel::Low),
-            AdiDigitalOut::with_initial_level(peripherals.adi_f, LogicLevel::Low),
-        ),
+        hood: AdiDigitalOut::with_initial_level(peripherals.adi_e, LogicLevel::Low),
+        midgoal: AdiDigitalOut::with_initial_level(peripherals.adi_f, LogicLevel::Low),
     };
 
     robot.compete().await;
