3fd9a5c7032473ea252b7c77baf353df6ec9699f
the grass <thegrassalt1@gmail.com>
2026-02-20 18:21:26 -0700

    idk

--- src/main.rs
+++ src/main.rs
@@ -35,12 +35,12 @@ pub const TRACK_WIDTH: f64 = 10.0;
 pub const WHEEL_DIAMETER: f64 = 3.25;
 pub const GEARING: f64 = 48.0/72.0;
 impl Robot {
-    const LINEAR_PID: Pid = Pid::new(6.5, 0.0, 1.29, None);
+    const LINEAR_PID: Pid = Pid::new(7.1, 0.0, 0.6, None);
     const LATERAL_PID: Pid = Pid::new(6.5, 0.0, 1.29, Some(2.0));   
     const ANGULAR_PID: AngularPid = AngularPid::new(6.5, 0.0, 0.59, None);
     const LINEAR_TOLERANCES: Tolerances = Tolerances::new()
-        .error(4.0)
-        .velocity(0.25)
+        .error(9.0)
+        .velocity(4.25)
         .duration(Duration::from_millis(15));
     const ANGULAR_TOLERANCES: Tolerances = Tolerances::new()
         .error(f64::to_radians(4.0))
@@ -72,29 +72,222 @@ impl Compete for Robot {
         // _ = dt.model.drive_arcade(0.0, 0.0);
         // basic.turn_to_heading(dt, 0.0.deg()).await;
         // basic.turn_to_heading(dt, 90.0.deg()).await;
-        basic.drive_distance_at_heading(dt, 24.0, 90.0.deg()).await;
-        basic.drive_distance_at_heading(dt, -12.0, 90.0.deg()).await;
-        basic.drive_distance_at_heading(dt, -12.0, 90.0.deg()).await;
+        // basic.drive_distance_at_heading(dt, 24.0, 90.0.deg()).await;
+        // basic.drive_distance_at_heading(dt, -12.0, 90.0.deg()).await;
+        // basic.drive_distance_at_heading(dt, -12.0, 90.0.deg()).await;
 
 
-        dt.tracking.set_heading(270.0.deg());
-        // Path
+        // dt.tracking.set_heading(270.0.deg());
+        // // Path
 
-        basic.drive_distance(dt, 24.448 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
-        basic.turn_to_heading(dt, (238.011 as f64).deg()).await;
-        basic.drive_distance(dt, 25.484 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
-        basic.turn_to_heading(dt, (270.019 as f64).deg()).await;
-        basic.drive_distance(dt, -(30.833 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
-        basic.turn_to_heading(dt, (50.826 as f64).deg()).await;
-        basic.drive_distance(dt, -(50.952 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
-        basic.turn_to_heading(dt, (90.302 as f64).deg()).await;
+        // basic.drive_distance(dt, 24.448 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
+        // basic.turn_to_heading(dt, (234.686 as f64).deg()).await;
+        // basic.drive_distance(dt, 31.883 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
+        // basic.turn_to_heading(dt, (270 as f64).deg()).await;
+        // basic.drive_distance(dt, -(49.916 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
+        // basic.turn_to_heading(dt, (67.824 as f64).deg()).await;
+        // basic.drive_distance(dt, -(60.847 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 0.7 as f64).await;
+        // basic.turn_to_heading(dt, (64.272 as f64).deg()).await;
 
-        // Path
+        // // Path
 
-        basic.drive_distance(dt, 31.9 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 1 as f64).await;
-        basic.turn_to_heading(dt, (90 as f64).deg()).await;
-        basic.drive_distance(dt, -(31.9 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 1 as f64).await;
-        basic.turn_to_heading(dt, (90.302 as f64).deg()).await;
+        // basic.drive_distance(dt, 31.9 as f64).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 1 as f64).await;
+        // basic.turn_to_heading(dt, (90 as f64).deg()).await;
+        // basic.drive_distance(dt, -(31.9 as f64)).with_linear_output_limit(Motor::V5_MAX_VOLTAGE * 1 as f64).await;
+        // basic.turn_to_heading(dt, (90.302 as f64).deg()).await;
+// // Evian PID
+// // Starting point: (59.37 in, -14.91 in)
+// // Point 2
+// dt.tracking.set_heading(270.00.deg());
+// // Point 2
+// basic
+//     .drive_distance(dt, 22.23)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// _ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.hood.set_high();
+// _ = self.midgoal.set_low();
+// // Point 3
+// basic
+//     .turn_to_heading(dt, 250.35.deg())
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// basic
+//     .drive_distance(dt, 30.05)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(0.33)
+//     .await;
+// _ = self.intake1.set_voltage(0.0);
+// _ = self.intake2.set_voltage(0.0);
+// // Point 4
+// basic
+//     .turn_to_heading(dt, 118.81.deg())
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// basic
+//     .drive_distance(dt, 46.13)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// // Point 5
+// basic
+//     .turn_to_heading(dt, 90.00.deg())
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// basic
+//     .drive_distance(dt, -66.69)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// _ = self.matchload.toggle();
+// _ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.hood.set_low();
+// _ = self.midgoal.set_low();
+// // Point 6
+// basic
+//     .turn_to_heading(dt, 90.00.deg())
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// basic
+//     .drive_distance(dt, 46.48)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// _ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.hood.set_high();
+// _ = self.midgoal.set_low();
+// // Point 7
+// basic
+//     .turn_to_heading(dt, 270.00.deg())
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// basic
+//     .drive_distance(dt, 46.48)
+//     .with_timeout(Duration::from_millis(2000))
+//     .with_linear_output_limit(1.0)
+//     .await;
+// _ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+// _ = self.hood.set_low();
+// _ = self.midgoal.set_low();
+// Evian PID
+// Starting point: (59.37 in, -14.91 in)
+// Point 2
+dt.tracking.set_heading(269.37.deg());
+// Point 2
+basic
+    .drive_distance(dt, 22.99)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(0.99)
+    .await;
+_ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.hood.set_high();
+_ = self.midgoal.set_low();
+// Point 3
+basic
+    .turn_to_heading(dt, 239.97.deg())
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+basic
+    .drive_distance(dt, 31.81/2.0)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(0.33)
+    .await;
+_ = self.matchload.toggle();
+basic
+    .drive_distance(dt, 31.81/2.0)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(0.33)
+    .await;
+_ = self.matchload.toggle();
+
+_ = self.intake1.set_voltage(0.0);
+_ = self.intake2.set_voltage(0.0);
+// Point 4
+basic
+    .turn_to_heading(dt, 98.97.deg())
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+basic
+    .drive_distance(dt, 38.88)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+// Point 5
+basic
+    .turn_to_heading(dt, 180.00.deg())
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+basic
+    .drive_distance(dt, 10.11)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+// Point 6
+basic
+    .turn_to_heading(dt, 90.00.deg())
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+basic
+    .drive_distance(dt, -50.53)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+_ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.hood.set_low();
+_ = self.midgoal.set_low();
+sleep(Duration::from_millis(4000)).await;
+
+
+
+
+// Evian PID
+// Starting point: (9.85 in, -45.47 in)
+// Point 2
+dt.tracking.set_heading(91.19.deg());
+// Point 2
+_ = self.matchload.set_high();
+basic
+    .drive_distance(dt, 89.77)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(0.67)
+    .await;
+_ = self.hood.set_high();
+_ = self.midgoal.set_low();
+_ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+
+sleep(Duration::from_millis(4000)).await;
+basic
+    .turn_to_heading(dt, 89.84.deg())
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;     
+basic
+    .drive_distance(dt, -91.96)
+    .with_timeout(Duration::from_millis(2000))
+    .with_linear_output_limit(1.0)
+    .await;
+_ = self.intake1.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.intake2.set_voltage(Motor::V5_MAX_VOLTAGE);
+_ = self.hood.set_low();
+_ = self.midgoal.set_low();
+sleep(Duration::from_millis(4000)).await;
 
 
 
@@ -117,6 +310,11 @@ impl Compete for Robot {
         
         loop {
             let state = self.controller.state().unwrap_or_default();
+
+            if state.button_left.is_pressed() && state.button_right.is_pressed() {
+                self.autonomous().await;
+                continue;
+            }
 
             _ = self
                 .drivetrain
@@ -211,7 +409,7 @@ async fn main(peripherals: Peripherals) {
             Differential::from_shared(left_motors.clone(), right_motors.clone()),
             WheeledTracking::forward_only(
                 (0.0, 0.0),
-                270.0.deg(),
+                90.0.deg(),
                 [
                     TrackingWheel::new(left_motors, WHEEL_DIAMETER, TRACK_WIDTH/2.0, Some(GEARING)),
                     TrackingWheel::new(right_motors, WHEEL_DIAMETER, TRACK_WIDTH/2.0, Some(GEARING)),
